eca306095ee9cd27afd43a9b596de5ec864cc809
Saskeli <tuomionpupu@gmail.com>
2016-02-27 14:20:21 +0200

    Removed unused methods from Database

--- src/main/java/fi/heviweight/forum/db/Database.java
+++ src/main/java/fi/heviweight/forum/db/Database.java
@@ -12,19 +12,6 @@ public class Database {
         this.connection = DriverManager.getConnection(address);
     }
 
-    public <T> List<T> queryAndCollect(String query, Collector col) throws SQLException {
-//        System.out.println("Terve!");
-        List<T> rows = new ArrayList<>();
-        Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-            rows.add((T) col.collect(rs));
-        }
-        rs.close();
-        stmt.close();
-        return rows;
-    }
-
     public <T> List<T> queryAndCollect(PreparedStatement stmt, Collector col) throws SQLException {
         List<T> rows = new ArrayList<>();
         try (ResultSet rs = stmt.executeQuery()) {
@@ -34,13 +21,6 @@ public class Database {
         }
         stmt.close();
         return rows;
-    }
-
-    public void execute(String statement) throws SQLException {
-        Statement stmt = connection.createStatement();
-        int changes = stmt.executeUpdate(statement);
-        System.out.println("Kyselyn vaikuttamia rivejä: " + changes);
-        stmt.close();
     }
 
     public void execute(PreparedStatement stmt) throws SQLException {
