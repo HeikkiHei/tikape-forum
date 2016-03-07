a7563f36514317cc95bf5671b5e66231c2abc0d7
Saskeli <tuomionpupu@gmail.com>
2016-03-07 12:03:09 +0200

    query fix

--- src/main/java/fi/heviweight/forum/pojo/Forum.java
+++ src/main/java/fi/heviweight/forum/pojo/Forum.java
@@ -3,19 +3,15 @@ package fi.heviweight.forum.pojo;
 public class Forum {
 
     private final int boardId;
-    private final int topicId;
     private final String boardName;
     private final String description;
-    private final String lastThread;
     private final String timestamp;
     private final int messageCount;
 
-    public Forum(int boardId, int topicId, String boardName, String description, String lastThread, String timestamp, int messageCount) {
+    public Forum(int boardId, String boardName, String description, String timestamp, int messageCount) {
         this.boardId = boardId;
-        this.topicId = topicId;
         this.boardName = boardName;
         this.description = description;
-        this.lastThread = lastThread;
         this.timestamp = timestamp.split("\\.")[0];
         this.messageCount = messageCount;
     }
@@ -32,16 +28,8 @@ public class Forum {
         return description;
     }
 
-    public String getLastThread() {
-        return lastThread;
-    }
-
     public String getTimestamp() {
         return timestamp;
-    }
-
-    public int getTopicId() {
-        return topicId;
     }
 
     public int getBoardId() {
--- src/main/java/fi/heviweight/forum/pojo/ForumDao.java
+++ src/main/java/fi/heviweight/forum/pojo/ForumDao.java
@@ -15,23 +15,20 @@ public class ForumDao {
     public List<Forum> findAll() throws SQLException {
         try (Connection con = db.getConnection()) {
             PreparedStatement stmt = con.prepareStatement(
-                    "SELECT board.id AS boardId, "
-                    + "board.name AS boardName, board.description AS boardDesc, "
-                    + "topic.id As topicId, topic.name AS topicName, "
-                    + "COUNT(Post.id) AS Viesteja, "
-                    + "post.timestamp AS Viimeisin "
+                    "SELECT b.id AS boardId, b.name AS boardName, "
+                    + "b.description AS boardDesc, t.Viesteja, "
+                    + "t.Viimeisin FROM (SELECT board.id AS boardId, " 
+                    + "COUNT(Post.id) AS Viesteja, " 
+                    + "MAX(post.TIMESTAMP) AS Viimeisin " 
                     + "FROM board, Topic, post "
-                    + "WHERE topic.board_id = board.id "
-                    + "AND topic.id = post.topic_id "
-                    + "GROUP BY board.id "
-                    + "ORDER BY post.timestamp DESC;");
+                    + "WHERE topic.board_id = board.id " 
+                    + "AND topic.id = post.topic_id " 
+                    + "GROUP BY board.id) t RIGHT JOIN board b ON t.boardId = b.id;");
             return db.queryAndCollect(stmt, rs -> {
                 return new Forum(
                         rs.getInt("boardId"),
-                        rs.getInt("topicId"),
                         rs.getString("boardName"),
                         rs.getString("boardDesc"),
-                        rs.getString("topicName"),
                         rs.getString("Viimeisin"),
                         rs.getInt("Viesteja"));
             });
