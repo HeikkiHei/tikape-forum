80e84087e41596f5fab7135905ac1a580888eed8
Saskeli <tuomionpupu@gmail.com>
2016-03-07 13:36:09 +0200

    moar postgres

--- src/main/java/fi/heviweight/forum/pojo/TopicDao.java
+++ src/main/java/fi/heviweight/forum/pojo/TopicDao.java
@@ -43,16 +43,16 @@ public class TopicDao {
     public List<Topic> getTopic(int topicId) throws SQLException {
         try (Connection con = db.getConnection()) {
             PreparedStatement stmt = con.prepareStatement(
-                    "SELECT board.id AS boardId, "
-                    + "board.name AS boardName, topic.id As topicId, "
-                    + "topic.name AS topicName, COUNT(Post.id) AS Viesteja, "
-                    + "post.timestamp AS Viimeisin "
-                    + "FROM board, topic, post "
-                    + "WHERE board.id = topic.board_id "
-                    + "AND topic.id = ? "
-                    + "AND post.topic_id = ? "
-                    + "GROUP BY topic.name "
-                    + "ORDER BY post.timestamp DESC LIMIT 10;");
+                    "SELECT b.tId AS topicId, b.N AS topicName, "
+                    + "t.Ts AS Viimeisin, t.c AS Viesteja, " 
+                    + "b.Id AS boardId, b.bN AS boardName FROM (" 
+                    + "SELECT post.topic_id AS tId, COUNT(*) AS c, "
+                    + "MAX(timestamp) AS Ts FROM post WHERE post.topic_id = 1 "
+                    + "GROUP BY post.topic_id) t RIGHT JOIN (" 
+                    + "SELECT board.id AS Id, board.name AS bN, "
+                    + "topic.id AS tId, topic.name AS N " 
+                    + "FROM topic, board WHERE topic.board_id = board.id) b ON "
+                    + "b.tId = t.tId;");
             stmt.setInt(1, topicId);
             stmt.setInt(2, topicId);
             return db.queryAndCollect(stmt, rs -> {
